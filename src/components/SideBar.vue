327057ee5732d13ecb9d77b3b5d180e58ab31d81
Ian Michael Holl <ian.michael.holl@gmail.com>
2021-05-20 22:10:57 -0700

    rough layout

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="side-bar" style="width:25%">
-        <h1>HUNT NV</h1>
+        <table>
+        <tr>
+            <th></th>
+            <th><h2>HUNT NV</h2></th>
+        </tr>
+        </table>
         <table>
         <tr>
             <th></th>
@@ -13,6 +18,42 @@
         <tr>
             <td><input type="checkbox"></td>
             <td id="label">BIGHORN SHEEP</td>
+        </tr>
+        <tr>
+            <td><input type="checkbox"></td>
+            <td id="label">MOUNTAIN LION</td>
+        </tr>
+        <tr>
+            <td><input type="checkbox"></td>
+            <td id="label">ELK</td>
+        </tr>
+        </table>
+        <table>
+        <tr>
+            <th></th>
+            <th>WEAPON CLASS</th>
+        </tr>
+        <tr>
+            <td><input type="checkbox"></td>
+            <td id="label">ARCHERY</td>
+        </tr>
+        <tr>
+            <td><input type="checkbox"></td>
+            <td id="label">RIFLE</td>
+        </tr>
+        <tr>
+            <td><input type="checkbox"></td>
+            <td id="label">MUZZLE LOADER</td>
+        </tr>
+        </table>
+        <table>
+        <tr>
+            <th></th>
+            <th>QUOTA</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="range"></td>
         </tr>
         </table>
     </div>
@@ -27,20 +68,21 @@ h1, h3, li, td, th {
     color: white;
 }
 
-.side-bar {
-    background-color: black;
-    height: 100%;
+th {
+    font-size: 25px;
 }
 
 ul {
     list-style-type: none;
 }
 
-.species-label {
-    margin-left: 5%;
-}
-
 table {
     width: 100%;
+}
+
+.side-bar {
+    background-color: black;
+    height: 100%;
+    opacity: 80%;
 }
 </style>
